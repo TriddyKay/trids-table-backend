f1157bfb050b485e037c8041aee3d848218471fc
TriddyKay <tristankay93@gmail.com>
2021-08-11 16:31:13 +1000

    Initial commit

---
+++ src/main/kotlin/com/dinner/club/tridstable/TridsTableApplication.kt
@@ -0,0 +1,11 @@
+package com.dinner.club.tridstable
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TridsTableApplication
+
+fun main(args: Array<String>) {
+	runApplication<TridsTableApplication>(*args)
+}
---
+++ src/test/kotlin/com/dinner/club/tridstable/TridsTableApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dinner.club.tridstable
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TridsTableApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
